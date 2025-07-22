270fc1cf7480388b56087c4131b795d8c53bf601
Ashish Kumar Rai <91818860+ASHISHKURAI@users.noreply.github.com>
2025-07-23 02:35:14 +0530

    Add files via upload

---
+++ Standard Problems/Decimal To Binary.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std ;
+
+int main(){
+	
+	int num;
+	cin>>num;
+	
+	int rem,ans=0,mul=1;
+	
+	while (num>0){
+		
+		rem =num %2;
+		num=num/2;
+		
+		ans=rem*mul+ans;
+		
+		mul*=10;
+	}
+	
+	cout<<ans<<endl;
+}
